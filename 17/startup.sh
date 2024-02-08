722ff66eb11513e82547e39543740b01f6e4a7e7
Jayant Raj Pathak <jayant.r.pathak@gmail.com>
2024-02-08 03:14:40 +0000

    main commit

---
+++ 17/startup.sh
@@ -0,0 +1,6 @@
+docker-compose -f docker-compose.yml up -d
+#read -p "Press enter to continue"
+#docker-compose -f hbusa/docker-compose.yml up -d
+docker-compose -f sbrmala/docker-compose.yml up -d
+docker-compose -f ytwusa/docker-compose.yml up -d
+docker-compose -f proxy/docker-compose.yml up -d
