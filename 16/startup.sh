716d92e702981dc5d18586163bb39a06cea4d87d
Jayant <jayant.r.pathak@gmail.com>
2024-05-19 15:16:06 -0400

    commit

--- 16/startup.sh
+++ 16/startup.sh
@@ -3,6 +3,8 @@ docker-compose -f $(dirname $0)/docker-compose.yml up -d
 #read -p "Press enter to continue"
 #docker-compose -f $(dirname $0)/hbusa/docker-compose.yml up -d
 docker-compose -f $(dirname $0)/sbrmala/docker-compose.yml up -d
+docker-compose -f $(dirname $0)/imgenv/docker-compose.yml up -d
 docker-compose -f $(dirname $0)/modulesnow/docker-compose.yml up -d
-docker-compose -f $(dirname $0)/ytwusa/docker-compose.yml up -d
+docker-compose -f $(dirname $0)/distillery/docker-compose.yml up -d
+#docker-compose -f $(dirname $0)/ytwusa/docker-compose.yml up -d
 docker-compose -f $(dirname $0)/proxy/docker-compose.yml up -d
